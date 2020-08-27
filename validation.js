a906afecc3ad5c0e3c6d8691af82a867b59c9605
tushar2595 <44287313+tushar2595@users.noreply.github.com>
2020-08-27 19:01:06 +0530

    added name and phone in model

--- validation.js
+++ validation.js
@@ -3,7 +3,8 @@ const registerValidation = data => {
   const schema = {
     name: Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
+    password: Joi.string().min(6).required(),
+    phone: Joi.string().min(6).required()
   };
   return Joi.validate(data, schema);
 };
